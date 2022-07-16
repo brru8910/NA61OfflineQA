669bd2a1632b32759bc87b685b5290fde2f1d4f1
Brant Rumberger <brant.rumberger@cern.ch>
2022-07-16 16:20:28 +0200

    QA wiping script. Some other small changes.

--- scripts/CleanGEMBPDROOTFiles.sh
+++ scripts/CleanGEMBPDROOTFiles.sh
@@ -4,4 +4,4 @@
 rm /afs/cern.ch/user/n/na61qa/2022-p+T2K-OfflineQA/EOSDropDirectory/GEM-BPD/*.root
 
 #Clean out .png plots.
-rm /afs/cern.ch/user/n/na61qa/2022-p+T2K-OfflineQA/EOSDropDirectory/pdfMaker/plots/*.png
+rm /afs/cern.ch/user/n/na61qa/2022-p+T2K-OfflineQA/pdfMaker/plots/*.png
--- scripts/CleanROOTFiles.sh
+++ scripts/CleanROOTFiles.sh
@@ -4,4 +4,4 @@
 find /afs/cern.ch/user/n/na61qa/2022-p+T2K-OfflineQA/EOSDropDirectory/ -maxdepth 1 -name "*.root" -delete
 
 #Clean out .png plots.
-rm /afs/cern.ch/user/n/na61qa/2022-p+T2K-OfflineQA/EOSDropDirectory/pdfMaker/plots/*.png
+rm /afs/cern.ch/user/n/na61qa/2022-p+T2K-OfflineQA/pdfMaker/plots/*.png
